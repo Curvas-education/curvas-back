b2499b8139077195261468dde843b6301b145030
Gilberto Medeiros <gibamedeirosgc@gmail.com>
2024-03-06 21:03:08 -0300

    feat: base project architecture

---
+++ src/main/java/com/example/curvasbackmvp/CurvasBackMvpApplication.java
@@ -0,0 +1,13 @@
+package com.example.curvasbackmvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurvasBackMvpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurvasBackMvpApplication.class, args);
+	}
+
+}
