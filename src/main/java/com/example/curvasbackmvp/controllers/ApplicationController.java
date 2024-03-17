e491bff01e36d53117c486a1b27b99e37b8b336e
Gilberto Medeiros <gibamedeirosgc@gmail.com>
2024-03-17 09:56:02 -0300

    feat: swagger config to spring sec

--- src/main/java/com/example/curvasbackmvp/controllers/ApplicationController.java
+++ src/main/java/com/example/curvasbackmvp/controllers/ApplicationController.java
@@ -1,5 +1,16 @@
 package com.example.curvasbackmvp.controllers;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
+@RestController
+@RequestMapping("api/v1")
 public class ApplicationController {
-    // Controller apenas para exibição do swagger
+    @GetMapping
+    public RedirectView swagger() {
+        return new RedirectView("/swagger-ui/index.html");
+    }    // Controller apenas para exibição do swagger
 }
--- src/main/java/com/example/curvasbackmvp/infra/security/SecurityConfigurations.java
+++ src/main/java/com/example/curvasbackmvp/infra/security/SecurityConfigurations.java
@@ -27,7 +27,9 @@ public class SecurityConfigurations {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
-                         .requestMatchers(HttpMethod.GET, "/api/v1/**").permitAll()
+                         .requestMatchers(HttpMethod.GET, "/api/v1").permitAll()
+                         .requestMatchers(HttpMethod.GET, "/swagger-ui/**").permitAll()
+                         .requestMatchers(HttpMethod.GET, "/v3/api-docs/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/questions").hasRole("TEACHER")
                         .requestMatchers(HttpMethod.POST, "/questions").hasRole("TEACHER")
