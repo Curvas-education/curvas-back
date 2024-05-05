30e0494e240beb41f8e2e26bb1b35e7642bb2336
Gilberto Medeiros <gibamedeirosgc@gmail.com>
2024-05-05 20:12:32 -0300

    feat: change question output in exam detail dto

--- src/main/java/com/example/curvasbackmvp/models/exam/ExamDetails.java
+++ src/main/java/com/example/curvasbackmvp/models/exam/ExamDetails.java
@@ -6,8 +6,8 @@ import com.example.curvasbackmvp.models.question.Question;
 import java.time.LocalDateTime;
 import java.util.List;
 
-public record ExamDetails(String id, String name, String description, String author, List<Question> questionList, List<GroupDTO> groups, Integer points, LocalDateTime startTime, LocalDateTime endTime) {
+public record ExamDetails(String id, String name, String description, String author, Integer questionList, List<GroupDTO> groups, Integer points, LocalDateTime startTime, LocalDateTime endTime) {
     public ExamDetails(Exam exam) {
-        this(exam.getId(), exam.getName(), exam.getDescription(), exam.getTeacher().getName(), exam.getQuestions(), exam.getGroups().stream().map(GroupDTO::new).toList(), exam.getPoints(), exam.getStartTime(), exam.getEndTime());
+        this(exam.getId(), exam.getName(), exam.getDescription(), exam.getTeacher().getName(), exam.getQuestions().size(), exam.getGroups().stream().map(GroupDTO::new).toList(), exam.getPoints(), exam.getStartTime(), exam.getEndTime());
     }
 }
