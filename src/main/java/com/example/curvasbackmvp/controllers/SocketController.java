cba794b1aef111d15ae34f88b0323a6428b6adca
Gilberto Medeiros <gibamedeirosgc@gmail.com>
2024-08-05 13:30:39 -0300

    feat: Ignore socket.io files

--- src/main/java/com/example/curvasbackmvp/controllers/SocketController.java
+++ src/main/java/com/example/curvasbackmvp/controllers/SocketController.java
@@ -1,69 +1,69 @@
-package com.example.curvasbackmvp.controllers;
-import com.corundumstudio.socketio.AckRequest;
-import com.corundumstudio.socketio.SocketIOClient;
-import com.corundumstudio.socketio.SocketIOServer;
-import com.corundumstudio.socketio.listener.ConnectListener;
-import com.corundumstudio.socketio.listener.DataListener;
-import com.corundumstudio.socketio.listener.DisconnectListener;
-import lombok.extern.log4j.Log4j2;
-import org.aspectj.bridge.Message;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-@Component
-@Log4j2
-public class SocketController {
-    @Autowired
-    private SocketIOServer socketServer;
-
-    void SocketIOController(SocketIOServer socketServer){
-        this.socketServer=socketServer;
-
-        this.socketServer.addConnectListener(onUserConnectWithSocket);
-        this.socketServer.addDisconnectListener(onUserDisconnectWithSocket);
-
-        /**
-         * Here we create only one event listener
-         * but we can create any number of listener
-         * messageSendToUser is socket end point after socket connection user have to send message payload on messageSendToUser event
-         */
-        this.socketServer.addEventListener("messageSendToUser", Message.class, onSendMessage);
-
-    }
-
-    public ConnectListener onUserConnectWithSocket = new ConnectListener() {
-        @Override
-        public void onConnect(SocketIOClient socketIOClient) {
-            log.info("Perform operation on user connect in controller");
-        }
-    };
-
-    public DisconnectListener onUserDisconnectWithSocket = new DisconnectListener() {
-        @Override
-        public void onDisconnect(SocketIOClient client) {
-            log.info("Perform operation on user disconnect in controller");
-        }
-    };
-
-    public DataListener<Message> onSendMessage = new DataListener<Message>() {
-        @Override
-        public void onData(SocketIOClient client, Message message, AckRequest acknowledge) throws Exception {
-
-            /**
-             * Sending message to target user
-             * target user should subscribe the socket event with his/her name.
-             * Send the same payload to user
-             */
-
-            log.info(message.getMessage()+" user send message to user "+message.getDetails()+" and message is "+message.getMessage());
-//            socketServer.getBroadcastOperations().sendEvent(message.GET(),client, message);
-            System.out.println(message.getID());
-
-            /**
-             * After sending message to target user we can send acknowledge to sender
-             */
-            acknowledge.sendAckData("Message send to target user successfully");
-        }
-    };
-
-}
+//package com.example.curvasbackmvp.controllers;
+//import com.corundumstudio.socketio.AckRequest;
+//import com.corundumstudio.socketio.SocketIOClient;
+//import com.corundumstudio.socketio.SocketIOServer;
+//import com.corundumstudio.socketio.listener.ConnectListener;
+//import com.corundumstudio.socketio.listener.DataListener;
+//import com.corundumstudio.socketio.listener.DisconnectListener;
+//import lombok.extern.log4j.Log4j2;
+//import org.aspectj.bridge.Message;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//@Log4j2
+//public class SocketController {
+//    @Autowired
+//    private SocketIOServer socketServer;
+//
+//    void SocketIOController(SocketIOServer socketServer){
+//        this.socketServer=socketServer;
+//
+//        this.socketServer.addConnectListener(onUserConnectWithSocket);
+//        this.socketServer.addDisconnectListener(onUserDisconnectWithSocket);
+//
+//        /**
+//         * Here we create only one event listener
+//         * but we can create any number of listener
+//         * messageSendToUser is socket end point after socket connection user have to send message payload on messageSendToUser event
+//         */
+//        this.socketServer.addEventListener("messageSendToUser", Message.class, onSendMessage);
+//
+//    }
+//
+//    public ConnectListener onUserConnectWithSocket = new ConnectListener() {
+//        @Override
+//        public void onConnect(SocketIOClient socketIOClient) {
+//            log.info("Perform operation on user connect in controller");
+//        }
+//    };
+//
+//    public DisconnectListener onUserDisconnectWithSocket = new DisconnectListener() {
+//        @Override
+//        public void onDisconnect(SocketIOClient client) {
+//            log.info("Perform operation on user disconnect in controller");
+//        }
+//    };
+//
+//    public DataListener<Message> onSendMessage = new DataListener<Message>() {
+//        @Override
+//        public void onData(SocketIOClient client, Message message, AckRequest acknowledge) throws Exception {
+//
+//            /**
+//             * Sending message to target user
+//             * target user should subscribe the socket event with his/her name.
+//             * Send the same payload to user
+//             */
+//
+//            log.info(message.getMessage()+" user send message to user "+message.getDetails()+" and message is "+message.getMessage());
+////            socketServer.getBroadcastOperations().sendEvent(message.GET(),client, message);
+//            System.out.println(message.getID());
+//
+//            /**
+//             * After sending message to target user we can send acknowledge to sender
+//             */
+//            acknowledge.sendAckData("Message send to target user successfully");
+//        }
+//    };
+//
+//}
